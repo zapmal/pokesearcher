af4be3b67267f19fe45880f314d16178bf005727
Zondazx <mzxgd16@gmail.com>
2021-01-15 12:18:33 -0400

    refactor: add dotenv

--- client/src/components/OfficialArtwork.jsx
+++ client/src/components/OfficialArtwork.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+require('dotenv').config();
 
 const OfficialArtwork = ({ pokemonID, width }) => {
-  const imagesEndpoint = 'https://pokeres.bastionbot.org/images/pokemon';
+  const imagesEndpoint = process.env.OFFICIAL_ARTWORK_ENDPOINT;
   const source = `${imagesEndpoint}/${pokemonID}.png`
 
   return <img src={source} alt={pokemonID} width={width} />
